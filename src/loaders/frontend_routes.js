8c719d38aa1162e97861298443615817574575ba
purposelessness <purposelessness@ya.ru>
2023-09-23 12:57:00 +0300

    feat(front): index.html

--- src/loaders/frontend_routes.js
+++ src/loaders/frontend_routes.js
@@ -1,5 +1,8 @@
 export default (app) => {
+  app.get('/', (req, res) => {
+    res.render('index', { title: 'Library' });
+  });
   app.get('/book', (req, res) => {
     res.render('book_index');
-  })
+  });
 }
