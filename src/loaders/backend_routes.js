9a6d565f082e9a89bd32820f20601fd11c6ac0ca
purposelessness <purposelessness@ya.ru>
2023-09-26 23:01:53 +0300

    fix(back): error handler

--- src/loaders/backend_routes.js
+++ src/loaders/backend_routes.js
@@ -3,4 +3,12 @@ import bookRepositoryModule
 
 export default (app) => {
   app.use('/api/book-repository', bookRepositoryModule.router);
+
+  // Error handler
+  app.use((err, req, res, next) => {
+    console.error(err);
+    let status = err.code || 500;
+    res.status(status).
+        send(status === 500 ? 'Internal Server Error' : err.message);
+  });
 };
